74e6f19cc62dbf70391311e5439c5a4fccf9b777
Owlsnap <alexblom8@gmail.com>
2025-10-01 18:28:48 +0200

    Fix container centering with responsive padding and box-sizing

--- src/components/PricePerUnitCalculator.jsx
+++ src/components/PricePerUnitCalculator.jsx
@@ -242,7 +242,8 @@ export default function PricePerUnitCalculator() {
           maxWidth: '1200px', 
           margin: '0 auto',
           width: '100%',
-          padding: '0 0.5rem'
+          padding: window.innerWidth < 768 ? '0 1rem' : '0 2rem',
+          boxSizing: 'border-box'
         }}>
         <div
           style={{
